81aa181b8271ce91c6ce834675efed03d36fb05c
kvaisman90-spec <kvaisman90@gmail.com>
2026-08-25 18:08:26 +0300

    Update service worker cache name to v8

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'adastra-app-v7';
+const CACHE_NAME = 'adastra-app-v8';
 const ASSETS = [
   '/',
   '/index.html',
@@ -20,9 +20,8 @@ self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
       Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
-    )
+    ).then(() => self.clients.claim())
   );
-  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
